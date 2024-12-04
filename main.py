4f69ff7e5fe31a183cefdb3d440f37246c4fb886
unknown <enaphinphat@wsutech.edu>
2024-12-04 10:30:54 -0600

    committment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from drink import Drink # Drink class imported from the drink module.
 from order import Order # Order class imported from the order module.
+from food import Food # Food class imported from the food module.
 
 drink1 = Drink(base='sprite', price=3.50, size='small') 
 # First drink object is created with the base, price, and size.
@@ -15,9 +16,20 @@ drink2.add_flavors('strawberry')
 drink2.add_flavors('strawberry')
 drink2.add_flavors('strawberry')
 
+# Food order that will print the item and price on the receipt.
+food1 = Food(type='onion rings') # Added onions rings which should be $1.75.
+food2 = Food(type='nacho chips') # Added nacho chips which should be $1.90.
+# Whipped cream as the toppings which should be free.
+food1.add_topping('whipped cream') 
+food2.add_topping('whipped cream')
+
+
+
 order = Order() # Creates an order object for drinks to be added to.
 order.add_item(drink1) # Drink1 added to the order.
 order.add_item(drink2) # Drink 2 added to the order.
+order.add_item(food1)
+order.add_item(food2)
 
 print("Item Order:") 
 for item in order.get_items():
