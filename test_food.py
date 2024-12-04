7af5422d9aaf65ef5483f1b6afdb75002c0e91a7
unknown <enaphinphat@wsutech.edu>
2024-12-03 20:10:19 -0600

    committment

--- test_food.py
+++ test_food.py
@@ -1,18 +1,31 @@
-import unittest
-from food import Food
+"""
+Unit testing for the Food Class.
+"""
+
+import unittest # Imports the unittest module for making tests.
+from food import Food # Food class imported from the food module.
 
 class TestFood(unittest.TestCase):
+    """
+    Sets up a Food instance.
+    """
     def setUp(self):
-        self.food = Food(type='onion rings')
+        self.food = Food(type='onion rings') # Testing one of the food from Food.py.
 
     def test_initilization(self):
-        self.assertEqual(self.food.get_type(), 'onion rings')
-        self.assertEqual(self.food.get_total(), 1.75)
+        """
+        Tests the food initilization process and then verifies the food price and status.
+        """
+        self.assertEqual(self.food.get_type(), 'onion rings') # Grabbing onion rings from the list.
+        self.assertEqual(self.food.get_total(), 1.75) # Testing to make sure the correct price is added.
 
     def test_add_topping(self):
-        self.food.add_topping('whipped cream')
-        self.assertIn('whipped cream', self.food.get_toppings())
-        self.assertEqual(self.food.get_total(), 1.75)
+        """
+        Testing the add topping process and makes sure that price and status are valid.
+        """
+        self.food.add_topping('whipped cream') # The topping added is whipped cream.
+        self.assertIn('whipped cream', self.food.get_toppings()) # Pushes whipped cream into the order.
+        self.assertEqual(self.food.get_total(), 1.75) # Validates the price of the topping.
 
 if __name__ == '__main__':
     unittest.main()
